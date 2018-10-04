9e8f24222e77fa0bb7fad9d662a6c871df6e22c9
Andrew Koidan <deathangel908@gmail.com>
2018-10-04 15:45:07 +0300

    t

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-console.log('аыsd sd sd в');
-
-//
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         11 sd  as  df 11
-//       </div>);
-//   }
-// }
-//
-// ReactDOM.render(<App/>, document.getElementById('app'));
+console.log('Change this entry to something else, hit save and  browser page should update automatically as it does with --hot flag');
